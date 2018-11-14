792c946f84b915b7f8a0c192be09a8c4e04a8009
Jerkow <jerome.kowalski@student-cs.fr>
2018-11-14 11:13:11 +0100

    API features

--- twitterPredictor/API.py
+++ twitterPredictor/API.py
@@ -1,9 +1,21 @@
 import twitter_collect.twitter_collection_setup as connect
 
-def collect():
+
+def collect(query):
     connexion = connect.twitter_setup()
-    tweets = connexion.search("Emmanuel Macron", language="french", rpp=100)
+    tweets = connexion.search(q=str(query), language="french", rpp=100)
     for tweet in tweets:
         print(tweet.text)
 
-collect()
+
+#collect('CentraleSupelec')
+
+def collect_by_user(user_id):
+    connexion = connect.twitter_setup()
+    statuses = connexion.user_timeline(id = user_id, count = 2)
+    for status in statuses:
+        print(status.text)
+    return statuses
+
+collect_by_user('@realDonaldTrump')
+
